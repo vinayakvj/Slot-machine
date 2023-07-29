45345ae852572372a743bd954f7deb42e564b707
vinayak <vinayakjay12@gmail.com>
2023-07-29 16:10:07 +0530

    progress

--- project.js
+++ project.js
@@ -1,12 +1,14 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-  const depAmount = prompt("Enter deposit amount: ");
-  const depositAmount = parseFloat(depAmount);
-
-  console.log(depositAmount - 10);
-  if (isNaN(depositAmount) || depositAmount <= 0) {
-    console.log("Amount Invalid, Try again.");
+  while (true) {
+    const depAmount = prompt("Enter deposit amount: ");
+    const depositAmount = parseFloat(depAmount);
+    if (isNaN(depositAmount) || depositAmount <= 0) {
+      console.log("Amount Invalid, Try again.");
+    } else {
+      return depositAmount;
+    }
   }
 };
 

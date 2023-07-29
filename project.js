49377c5162e202d4f2e7e93be32f4639c52ae159
vinayak <vinayakjay12@gmail.com>
2023-07-29 23:24:37 +0530

    progress

--- project.js
+++ project.js
@@ -12,4 +12,30 @@ const deposit = () => {
   }
 };
 
-deposit();
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter Number of lines (1-3): ");
+    const numberOfLines = parseFloat(lines);
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Amount Invalid, Try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const Bet = (Balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > Balance / lines) {
+      console.log("Amount Invalid, Try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+let Balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = Bet(Balance, numberOfLines);
